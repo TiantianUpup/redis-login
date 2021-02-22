430e9cd96245c85324eabef8c992319c1ebcfac5
hetiantian <969795191@qq.com>
2021-02-22 14:51:15 +0800

    [update]添加@Configuration注解

--- src/main/java/com/h2t/study/config/InterceptorConfig.java
+++ src/main/java/com/h2t/study/config/InterceptorConfig.java
@@ -2,6 +2,7 @@ package com.h2t.study.config;
 
 import com.h2t.study.interceptor.AuthenticateInterceptor;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -12,6 +13,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  * @version 1.0
  * @Date 2021/02/21 14:57
  */
+@Configuration
 public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
